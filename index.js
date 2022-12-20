093b8c161d1ec26b0a5171406289ae4a610f9885
Zakiya-Abass <zakiyaabass@icloud.com>
2022-12-21 00:07:55 +0300

    loops

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(names, eventName) {
+  let newArray = [];
+  for (let i = 0; i < names.length; i++)
+    newArray.push(
+      `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`
+    );
+  return newArray;
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "suprise");
+//while loop
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num--);
+  }
+}
